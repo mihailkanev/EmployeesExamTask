e065c17b50583728cf9b03b532bad65984649515
Your Name <you@example.com>
2023-12-22 04:55:10 +0200

    Make it public and Component

--- src/main/java/com/sirma/exam/validations/FilePathValidator.java
+++ src/main/java/com/sirma/exam/validations/FilePathValidator.java
@@ -2,18 +2,22 @@ package com.sirma.exam.validations;
 
 import com.sirma.exam.model.Employee;
 import jakarta.validation.ValidationException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
+@Component
 public class FilePathValidator {
-    private void validateFilePath(String filePath) throws IOException {
-        if (!filePath.isEmpty()) {
+
+    public void validateFilePath(String filePath) throws IOException {
+        if (filePath.isEmpty()) {
             throw new IOException("File path is empty");
         }
     }
 
-    private void validateCsvData(List<Employee> employees) throws ValidationException {
+    public void validateCsvData(List<Employee> employees) throws ValidationException {
         if (employees == null || employees.isEmpty()) {
             throw new ValidationException("CSV data is empty");
         }
