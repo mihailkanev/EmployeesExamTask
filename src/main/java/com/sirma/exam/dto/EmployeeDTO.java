f1c626724007a1b818ae57bd839363c94ab92c1d
Your Name <you@example.com>
2023-12-22 04:37:36 +0200

    Adding Validation to EmployeeDTO

--- src/main/java/com/sirma/exam/dto/EmployeeDTO.java
+++ src/main/java/com/sirma/exam/dto/EmployeeDTO.java
@@ -1,6 +1,9 @@
 package com.sirma.exam.dto;
 
+import jakarta.validation.constraints.Positive;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -11,12 +14,28 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class EmployeeDTO {
+
+    @NotNull
+    @Positive(message = "empId1 must be positive")
     private Long empId1;
+
+    @NotNull
+    @Positive(message = "empId2 must be positive")
     private Long empId2;
+
+    @NotNull
+    @Positive(message = "projectId must be positive")
     private Long projectId;
+
+    @NotNull
     private LocalDate startDate;
+
+    @NotNull
     private LocalDate endDate;
+
     private Duration duration;
+
+    @Positive(message = "overlapDays must be positive")
     private long overlapDays;
 
 }
